195ea9e0d4ecb9e3d1a8a44e0af69f68420aa314
Eric Zhang <ericspring08@gmail.com>
2021-07-12 11:58:05 -0500

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {Typography, Button} from '@material-ui/core';
 import './App.css';
 import axios from 'axios';
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
 import {useState, useEffect} from 'react';
 function App() {
   const [joke, setJoke] = useState('');
@@ -9,7 +11,7 @@ function App() {
       setJoke(response.data.jokes)
     })
   }
-  useEffect(() => {getJoke("fat")}, []
+  useEffect(() => {getJoke("/")}, []
   )
   return (
     <div className="App">
@@ -17,6 +19,7 @@ function App() {
         <Typography style = {{"font-size": "150px", margin: "100px", "font-family": "monospace"}}>YO MOMMA JOKES</Typography>
       </center>  
       <Typography variant = "h1">Yo momma so...</Typography>
+      <Button onClick = {() => {getJoke("/")}}>All</Button>
       <Button onClick = {() => {getJoke("fat")}}>Fat</Button>
       <Button onClick = {() => {getJoke("ugly")}}>Ugly</Button>
       <Button onClick = {() => {getJoke("nasty")}}>Nasty</Button>
@@ -27,6 +30,11 @@ function App() {
       <Button onClick = {() => {getJoke("skinny")}}>Skinny</Button>
       <Button onClick = {() => {getJoke("tall")}}>Tall</Button>
       <Typography variant = "h5">{joke}</Typography>
+      <CopyToClipboard text={joke}>
+        <Button color="white" variant = "extended" aria-label="add">
+          <Typography>Copy</Typography>
+        </Button>
+      </CopyToClipboard>
       
     </div>
   );
